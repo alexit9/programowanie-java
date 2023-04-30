c3d7ab998e50768e1419741379c6cb029c4f749f
alexit9 <132204761+alexit9@users.noreply.github.com>
2023-04-30 14:24:09 +0200

    Add files via upload

---
+++ Hydroplan.java
@@ -0,0 +1,8 @@
+public class Hydroplan implements Plywa, Lata {
+    public void plyn(){
+        System.out.println("Hydroplan plywa");
+    }
+    public void lec(){
+        System.out.println("Hydroplan lata");
+    }
+}
